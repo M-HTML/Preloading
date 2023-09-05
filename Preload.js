543a63b0fa9424633688ebc14b64d1dc24342ed2
a person <72956230+apersongithub@users.noreply.github.com>
2023-09-05 19:15:05 -0400

    Update Preload.js
    expanded it to work for more elements

--- Preload.js
+++ Preload.js
@@ -1,44 +1,60 @@
-        function preloadImages() {
-            var images = document.getElementsByTagName('img');
-            var imageUrls = [];
+function preloadImages() {
+    var imageUrls = [];
 
-            for (var i = 0; i < images.length; i++) {
-                imageUrls.push(images[i].src);
-            }
+    var images = document.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+        imageUrls.push(images[i].src);
+    }
 
-            var styleSheets = document.styleSheets;
-            for (var j = 0; j < styleSheets.length; j++) {
-                var rules = styleSheets[j].cssRules || styleSheets[j].rules;
+    var links = document.getElementsByTagName('a');
+    for (var j = 0; j < links.length; j++) {
+        var href = links[j].href;
+        if (href && href !== '#') {
+            imageUrls.push(href);
+        }
+    }
 
-                for (var k = 0; k < rules.length; k++) {
-                    var rule = rules[k];
+    var styleSheets = document.styleSheets;
+    for (var k = 0; k < styleSheets.length; k++) {
+        var rules = styleSheets[k].cssRules || styleSheets[k].rules;
 
-                    if (rule.style && rule.style.backgroundImage) {
-                        var matches = rule.style.backgroundImage.match(/url\(['"]?([^'")]+)['"]?\)/);
-                        if (matches && matches[1]) {
-                            imageUrls.push(matches[1]);
-                        }
-                    }
+        for (var l = 0; l < rules.length; l++) {
+            var rule = rules[l];
+
+            if (rule.style && rule.style.backgroundImage) {
+                var matches = rule.style.backgroundImage.match(/url\(['"]?([^'")]+)['"]?\)/);
+                if (matches && matches[1]) {
+                    imageUrls.push(matches[1]);
                 }
-            }
-
-            var preloadedImages = [];
-            var loadedCount = 0;
-
-            function imageLoaded() {
-                loadedCount++;
-
-                if (loadedCount === imageUrls.length) {
-                    console.warn('the images have been preloaded!');
-                }
-            }
-
-            for (var m = 0; m < imageUrls.length; m++) {
-                var img = new Image();
-                img.src = imageUrls[m];
-                img.onload = imageLoaded;
-                preloadedImages.push(img);
             }
         }
+    }
 
-        preloadImages();
+    var tables = document.getElementsByTagName('table');
+    for (var m = 0; m < tables.length; m++) {
+        var tableImages = tables[m].getElementsByTagName('img');
+        for (var n = 0; n < tableImages.length; n++) {
+            imageUrls.push(tableImages[n].src);
+        }
+    }
+
+    var preloadedImages = [];
+    var loadedCount = 0;
+
+    function imageLoaded() {
+        loadedCount++;
+
+        if (loadedCount === imageUrls.length) {
+            console.warn('The images have been preloaded!');
+        }
+    }
+
+    for (var p = 0; p < imageUrls.length; p++) {
+        var img = new Image();
+        img.src = imageUrls[p];
+        img.onload = imageLoaded;
+        preloadedImages.push(img);
+    }
+}
+
+preloadImages();
