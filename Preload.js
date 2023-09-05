b4e4696e1341b5dcade54bac7d1ec29cdc3d9650
a person <72956230+apersongithub@users.noreply.github.com>
2023-09-05 19:03:21 -0400

    Create Preload.js

---
+++ Preload.js
@@ -0,0 +1,45 @@
+        function preloadImages() {
+            var images = document.getElementsByTagName('img');
+            var imageUrls = [];
+
+            for (var i = 0; i < images.length; i++) {
+                imageUrls.push(images[i].src);
+            }
+
+            var styleSheets = document.styleSheets;
+            for (var j = 0; j < styleSheets.length; j++) {
+                var rules = styleSheets[j].cssRules || styleSheets[j].rules;
+
+                for (var k = 0; k < rules.length; k++) {
+                    var rule = rules[k];
+
+                    if (rule.style && rule.style.backgroundImage) {
+                        var matches = rule.style.backgroundImage.match(/url\(['"]?([^'")]+)['"]?\)/);
+                        if (matches && matches[1]) {
+                            imageUrls.push(matches[1]);
+                        }
+                    }
+                }
+            }
+
+            var preloadedImages = [];
+            var loadedCount = 0;
+
+            function imageLoaded() {
+                loadedCount++;
+
+                if (loadedCount === imageUrls.length) {
+                    console.log('the images have been preloaded!');
+                    //remove the consolelog if you want
+                }
+            }
+
+            for (var m = 0; m < imageUrls.length; m++) {
+                var img = new Image();
+                img.src = imageUrls[m];
+                img.onload = imageLoaded;
+                preloadedImages.push(img);
+            }
+        }
+
+        preloadImages();
